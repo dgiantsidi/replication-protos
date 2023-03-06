10f7204f154a101f095fe19d6d9dd1156df6c20b
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-03-06 18:34:25 +0000

    ..

--- test/common.h
+++ test/common.h
@@ -7,4 +7,7 @@ static const std::string kmartha = "129.215.165.53";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;
-static constexpr size_t kMsgSize = 16;
+static constexpr size_t kMsgSize = 64;
+
+static void sm_handler(int local_session, erpc::SmEventType, erpc::SmErrType,
+                       void *) {}
