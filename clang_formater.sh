32c0b703c4edc944f82d9c86fd4804667da91f9a
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-04-18 15:46:00 +0000

    clang formatter

--- clang_formater.sh
+++ clang_formater.sh
@@ -2,6 +2,7 @@
 
 clang-format -i ./test/*cc ./test/*h
 clang-format -i ./cr/*cc ./cr/*h
+clang-format -i ./bft-cr/*cc ./bft-cr/*h
 clang-format -i ./rf/*cc ./rf/*h
 clang-format -i ./alc/*cc ./alc/*h
 clang-format -i digital_signatures/*cc digital_signatures/*h
