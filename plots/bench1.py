5f06c7ac0697636ff2cb1571f72918b970114ac0
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-08-18 18:07:28 +0100

    ...

--- plots/bench1.py
+++ plots/bench1.py
@@ -10,21 +10,31 @@ plt.rcParams['hatch.linewidth'] = 2.5
 palette = sns.color_palette("pastel")
 
 
-plt.rcParams["figure.figsize"] = (10, 8)
+plt.rcParams["figure.figsize"] = (16, 8)
 plt.rcParams.update({'font.size': 40})
 
 fig, ax = plt.subplots()
 
-rsa_lat = [700, 1200, 800]
+rsa_lat_64 = [700, 1200, 800]
+rsa_lat_128 = [700, 1000, 400]
+rsa_lat_256 = [700, 800, 300]
+
 envs = ['CPU', 'SGX', 'U280']
 x = np.arange(len(envs))
-low = int(min(rsa_lat))
-high = int(max(rsa_lat))
-plt.ylim([math.ceil(low-0.5*(high-low)), math.ceil(high+0.5*(high-low))])
+low = int(min(rsa_lat_64))
+high = int(max(rsa_lat_64))
+plt.ylim([0, math.ceil(high+0.5*(high-low))])
+width = 0.10
+x2 = [r+width for r in x]
+x3 = [r+width for r in x2]
 
-
-ax.bar(envs, rsa_lat, width = 0.23, color= palette[3], hatch="o", edgecolor="black")
+ax.bar(x, rsa_lat_64, width, color= palette[3], hatch="o", edgecolor="black", label="64B")
+ax.bar(x2, rsa_lat_128, width, color= palette[4], hatch="X", edgecolor="black", label="128B")
+ax.bar(x3, rsa_lat_256, width, color= palette[2], hatch="-", edgecolor="black", label="256B")
 ax.set_ylabel("Latency (ns)")
+ax.set_xticks(x2)
+ax.set_xticklabels(envs)
 ax.set_title("RSA")
-plt.savefig('rsa.pdf',dpi=400)
-#plt.show()
+ax.legend()
+#plt.savefig('rsa.pdf',dpi=400)
+plt.show()
