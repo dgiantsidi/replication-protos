da5c628552fc29192f812a3cbe32f4fa972567a3
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-10-23 15:05:03 +0000

    mock_api for benchmakring the protos added

---
+++ tnic_mock_api/api.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "crypto/hmac_lib.h"
+#include <chrono>
+#include <thread>
+
+class tnic_api {
+public:
+  static std::tuple<std::vector<unsigned char>, size_t>
+  native_get_attestation(const unsigned char *data, size_t sz) {
+    return hmac_sha256(data, sz);
+  }
+
+  static std::tuple<std::vector<unsigned char>, size_t>
+  TEE_get_attestation(const unsigned char *data, size_t sz) {
+    using namespace std::literals::chrono_literals;
+    std::this_thread::sleep_for(50us); //@dimitra: from microbenchmarking
+
+    return hmac_sha256(data, sz);
+  }
+
+  static std::tuple<std::vector<unsigned char>, size_t>
+  FPGA_get_attestation(const unsigned char *data, size_t sz) {
+    using namespace std::literals::chrono_literals;
+    std::this_thread::sleep_for(50us); //@dimitra: from microbenchmarking
+
+    return hmac_sha256(data, sz);
+  }
+};
