d2dd49119f893aee06bed38a71033952347c771a
dgiantsidi <dimitra.giantsidi@gmail.com>
2023-12-05 03:44:28 +0000

    formatting issues

--- tnic_mock_api/test_api.cc
+++ tnic_mock_api/test_api.cc
@@ -5,19 +5,19 @@
 constexpr int nb_iterations = 10e6;
 constexpr int data_sz = 128;
 int main(void *) {
-	std::vector<uint8_t> data;
+  std::vector<uint8_t> data;
   for (auto i = 0ULL; i < data_sz; i++) {
     data.push_back(static_cast<uint8_t>(i));
   }
   for (auto i = 0ULL; i < nb_iterations; i++) {
-	auto res = tnic_api::native_get_attestation(data.data(), data.size());
-	auto hmac = std::get<0>(res);
-	auto att = tnic_api::native_get_attestation(data.data(), data.size());
-	auto exp_hmac = std::get<0>(res);
-	auto sz = std::get<1>(res);
-	if (::memcmp(hmac.data(), exp_hmac.data(), sz) != 0) {
-		std::cout << "Error\n";
-	}
+    auto res = tnic_api::native_get_attestation(data.data(), data.size());
+    auto hmac = std::get<0>(res);
+    auto att = tnic_api::native_get_attestation(data.data(), data.size());
+    auto exp_hmac = std::get<0>(res);
+    auto sz = std::get<1>(res);
+    if (::memcmp(hmac.data(), exp_hmac.data(), sz) != 0) {
+      std::cout << "Error\n";
+    }
   }
   std::cout << "#####\n";
   return 0;
